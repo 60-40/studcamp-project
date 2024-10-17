05242786fd0d739852e3d4ca8f20262ca2539237
Danich-One <vorotyntsev00@mail.ru>
2024-10-17 14:47:35 +0300

    yolo_server

---
+++ yolo_object_detection.py
@@ -0,0 +1,48 @@
+import cv2
+from ultralytics import YOLO
+
+# Load the trained model
+model_path = "nano_best.pt"
+model = YOLO(model_path)
+
+names = {0: 'basket', 1: 'button', 2: 'cube', 3: 'sphere'}
+
+# Function to process webcam feed in real-time
+def process_webcam():
+    # Open a connection to the webcam (0 is the default camera)
+    cap = cv2.VideoCapture('http://192.168.2.165:8080/?action=stream')
+    
+    # Check if the webcam is opened correctly
+    if not cap.isOpened():
+        print("Error: Could not open webcam.")
+        return
+    
+    while True:
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        if not ret:
+            print("Error: Failed to capture image.")
+            break
+        
+        # Run YOLO inference on the frame
+        results = model(frame)
+        for i in range(results[0].boxes.shape[0]):
+            print(names[int(results[0].boxes.cls[i])], 'x=', float(results[0].boxes.xywh[i][0]), 'y=', float(results[0].boxes.xywh[i][1]))
+
+        # Render the detections on the frame
+        processed_frame = results[0].plot()  # Draw bounding boxes, labels, etc.
+        
+        # Display the processed frame
+        cv2.imshow('YOLO Webcam Inference', processed_frame)
+        
+        # Break the loop if 'q' is pressed
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    
+    # Release the webcam and close OpenCV windows
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    print("Running YOLO on webcam. Press 'q' to quit.")
+    process_webcam()
